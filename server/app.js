4664d95c5d00db4d71d8f8c63b64256fef19a4f7
roman_ushakov <roman.ushakov.89@inbox.ru>
2020-05-23 17:56:01 +0300

    fix: enable serve assets for production only

--- server/app.js
+++ server/app.js
@@ -86,7 +86,9 @@ const createApp = (connection) => {
 
   setupLocalization();
 
-  app.use(serve(path.join(__dirname, '..', 'dist')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(serve(path.join(__dirname, '..', 'dist')));
+  }
 
   addRoutes(router);
 
